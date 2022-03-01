63b8e5b8607646fc8959eb05b1a6d665a700c609
“Kattster” <katt@katsite.com>
2022-02-28 23:13:16 -0600

    adding exercise

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log(8);
+// Numbers, Strings Booleans, Null, Undefined
+// + - * / % **
+console.log(5%4); console.log(6%5);
+console.log('I\'m Mary Poppins, Y\'all!"');
+console.log("Hawk "+"Eye");
+console.log("KatSeaton".length);
+console.log("Avengers: Endgame"[16]) // e
+console.log("\\");
+console.log(5** 4 - 9); // 616
+console.log("I".length % "Am".length) - "Iron".length - "Man".length;
+console.log("CAP"[1]); 
+console.log("VISION"[0]);
+console.log("OKOYE"[4]); 
+console.log( "NICK FURY"[0] );
+console.log("GROOT"[0]);
+console.log( "DOCTOR STRANGE"[13]);
+console.log( "THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+console.log("CAP"+ "VISION"+ "OKOYE"+ "NICK FURY"+ "GROOT"+ "DOCTOR STRANGE"+ "THOR"+ "STAR-LORD"[1]);
+
+
+letter="STAR-LORD"[0];
+
+// L ARRAY
+larray=["CAP", "VISION", "OKOYE", "NICK FURY", "GROOT", "DOCTOR STRANGE", "THOR", "STAR-LORD"];
+console.log(larray[0][1]+ larray[1][0]+ larray[2][4] + larray[3][0] + larray[4][0] + larray[5][13] + larray[6][3] + larray[7][0]);
+
+
+
+M=25
+C=4
+U=M+C
+
+console.log(U);
+
+
+
